5266e3b80972cf82c2aaddce185eb3afcec6bb1c
Usama Imdad <usamaimdadsian@gmail.com>
2025-03-30 11:40:43 +0500

    eshwf 3

--- boxmot/appearance/backends/base_backend.py
+++ boxmot/appearance/backends/base_backend.py
@@ -37,7 +37,11 @@ class BaseModelBackend:
 
     def get_crops(self, xyxys, img):
         h, w = img.shape[:2]
-        resize_dims = (128, 256)
+        if "lmbn" in self.model_name:
+            resize_dims = (128, 384)
+        else:
+            resize_dims = (128, 256)
+
         interpolation_method = cv2.INTER_LINEAR
         mean_array = torch.tensor([0.485, 0.456, 0.406], device=self.device).view(1, 3, 1, 1)
         std_array = torch.tensor([0.229, 0.224, 0.225], device=self.device).view(1, 3, 1, 1)
